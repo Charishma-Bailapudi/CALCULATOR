e2a9278944bdb6a2e2ab7dd261bb2ded59ddd3cf
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2022-08-06 17:19:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var output=document.getElementById('text2');
+var button=document.getElementsByTagName('input');
+var currentDisplay;
+for(i=0;i<button.length;i++)
+{
+    button[i].onclick=function(e){
+        var input =e.target.value
+        updateDisplay(input);
+        currentDisplay=output.value;
+    }
+}
+
+
+function updateDisplay(input)
+{
+    console.log(currentDisplay);
+            if(input=="C")
+            {
+                output.value="0";
+            }
+            else if(input!="C"&&input!="=")
+            {
+                output.value+=input;
+            }
+            else if(input=="=")
+            {
+                var display=output.value;
+                output.value=eval(display);
+            }
+}
